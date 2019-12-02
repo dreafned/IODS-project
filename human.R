0e768f7c07598db44471906910b25dd856a5b1f4
Salonen <rvsalone@ad.helsinki.fi>
2019-12-02 15:02:18 +0200

    week5

---
+++ human.R
@@ -0,0 +1,35 @@
+human <- read.table("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt", sep  =",", header = T)
+
+library(stringr)
+library(dplyr)
+library(tidyr)
+
+str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric()
+
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Life.Exp", "Edu.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+
+# select the 'keep' columns
+human <- select(human, one_of(keep))
+
+)# print out a completeness indicator of the 'human' data
+complete.cases(human)
+
+# print out the data along with a completeness indicator as the last column
+data.frame(human[-1], comp = complete.cases(human))
+
+# filter out all rows with NA values
+human_ <- filter(human, complete.cases(human)==TRUE)
+
+# define the last indice we want to keep
+last <- nrow(human_) - 7
+
+# choose everything until the last 7 observations
+human_ <- human_[1:last, ]
+
+# add countries as rownames
+rownames(human_) <- human_$Country
+
+# remove the Country variable
+human_ <- select(human, -Country)
+
+write.csv(human, file = "data/human.csv", row.names = TRUE)
